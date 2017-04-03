044fcabd1ef0ce4ce1a87b7f7dd3db1acd4ea9dd
csalzsieder <csalzsieder@inspirato.com>
2017-04-03 10:09:33 -0600

    adds stub for async calls to api's

--- src/PublicWebsite/src/modules/marketing/room-content.ts
+++ src/PublicWebsite/src/modules/marketing/room-content.ts
@@ -1,3 +1,18 @@
-﻿export class RoomContent {
+﻿import {EventAggregator} from 'aurelia-event-aggregator'
+import {autoinject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+
+@autoinject()
+export class RoomContent {
 	name = 'Room content component';
+
+	constructor(private messageBus: EventAggregator, private apiClient: HttpClient) {
+		this.messageBus.subscribe("RoomTypeIdsAvailable", response => {
+			this.makeApiRequest(response);
+		});
+	}
+
+	makeApiRequest(response) {
+		console.log('marketing', response);
+	}
 }
