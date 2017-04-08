2a98c6deb82c06b7c98e6176f816802ed9c0a0c4
csalzsieder <csalzsieder@inspirato.com>
2017-04-08 15:35:46 -0600

    makes rates api call

--- src/PublicWebsite/src/modules/rates/rates.ts
+++ src/PublicWebsite/src/modules/rates/rates.ts
@@ -8,44 +8,34 @@ export class Rates {
 
 	constructor(private messageBus: EventAggregator, private apiClient: HttpClient) {
 		this.messageBus.subscribe("RoomTypeIdsAvailable", response => {
+			console.info("sub to RoomTypeIdsAvailable");
 			this.makeApiRequest(response);
 		});
-
-//		this.apiClient.configure(config => {
-//			config
-//				.useStandardConfiguration()
-//				.withBaseUrl('http://localhost:54520/api/')
-//				.withDefaults({
-//					headers: {
-//						'Accept': 'application/json',
-//					}
-//				});
-//
-//		});
 	}
 
 	makeApiRequest(response) {
 		console.log('rates', response);
 
-		var idString = '';
+//		var idString = 'http://localhost:54520/api/roomTypeRates?';
+//
+//		response.map(function(value) {
+//			idString += 'ids=' + value + '&';
+//		});
 
-		response.map(function(value) {
-			idString += 'ids=' + value + '&';
-		});
+//		console.log(idString);
 
-		console.log(idString);
+		console.log("get rates: http");
+		let url = 'http://localhost:54520/api/roomTypeRates?ids=1&ids=3&ids=2&ids=4&checkin=8/1/2017&checkout=8/6/2017';
+		this.apiClient
+			.fetch(url)
+			.then(response => {
+				console.log("1: rates", response);
+				return response.json();
+			})
+			.then(data => {
+				console.log('2: rates', data);
 
-//		let url = 'roomTypeRates?ids=1&ids=3&ids=2&ids=4';
-//		this.apiClient
-//			.fetch(url)
-//			.then(response => {
-//				console.log("1", response);
-//				return response.json();
-//			})
-//			.then(data => {
-//				console.log('2', data);
-//				this.messageBus.publish("RoomTypeIdsAvailable", data);
-//			});
-		
+			});
+
 	}
 }
