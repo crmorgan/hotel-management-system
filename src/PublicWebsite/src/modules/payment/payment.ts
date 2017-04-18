87412d235a22efe124a7d2f7d9b42abe35b4ed64
csalzsieder <csalzsieder@inspirato.com>
2017-04-17 18:42:04 -0600

    adds missing files

---
+++ src/PublicWebsite/src/modules/payment/payment.ts
@@ -0,0 +1,37 @@
+﻿import {autoinject} from 'aurelia-framework';
+import {HttpClient, json} from 'aurelia-fetch-client';
+import {Events} from '../../messages/events';
+var uniqid = require('uniqid');
+import shoppingCart from "../../shoppingCart";
+
+@autoinject()
+export class Payments {
+	cardNumber;
+	cardExpiration;
+	cardType;
+
+	constructor(private httpClient: HttpClient) {
+	}
+
+	submit() {
+		let url = 'http://localhost:54626//api/payment';
+		let body = {
+			"paymentUuid": uniqid(),
+			"purchaseUuid": shoppingCart.reservationUuid,
+			"card": {
+				"number": this.cardNumber,
+				"typeId": this.cardType,
+				"expiration": this.cardExpiration
+			}
+		}
+
+		this.httpClient.fetch(url, {
+			method: 'POST',
+			body: json(body)
+		})
+		.then(response => response.json())
+		.then(data => {
+
+		});
+	}
+}
---
+++ src/PublicWebsite/src/modules/reservation/reservation-amount.ts
@@ -0,0 +1,34 @@
+﻿import {autoinject, bindable} from 'aurelia-framework';
+import {HttpClient, json} from 'aurelia-fetch-client';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {Events} from '../../messages/events';
+import shoppingCart from "../../shoppingCart";
+import {Router} from 'aurelia-router';
+
+@autoinject()
+export class ReservationAmount {
+	amount;
+
+	constructor(private httpClient: HttpClient, private messageBus: EventAggregator, private router: Router) {
+		this.messageBus.subscribe(Events.RatesSummaryFetched, message => {
+			this.amount = message.ammount;
+		});
+	}
+
+	submit() {
+		let url = 'http://localhost:54626//api/reservations';
+		let body = {
+			"reservationUuid": shoppingCart.reservationUuid,
+			"ammount": this.amount
+		}
+
+		this.httpClient.fetch(url,{
+			method: 'POST',
+			body: json(body)
+		})
+		.then(response => response.json())
+		.then(data => {
+
+		});
+	}
+}
