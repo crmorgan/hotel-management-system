720e33764f37ebf29d2ec2c48e530c43a4b07ab4
Chris Morgan <cmorgan@inspirato.com>
2017-06-08 19:58:02 -0600

    Adds reservation hold timeout to saga

---
+++ src/Reservations/Handlers/AbaondonReservationHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using Reservations.Data.Context;
+using Reservations.Messages.Commands;
+
+namespace Reservations.Handlers
+{
+	public class AbaondonReservationHandler : IHandleMessages<AbandonReservationCommand>
+	{
+		private readonly IReservationsContext _reservationsContext;
+		private static readonly ILog Log = LogManager.GetLogger<BookReservationHandler>();
+
+		public AbaondonReservationHandler(IReservationsContext reservationsContext)
+		{
+			_reservationsContext = reservationsContext;
+		}
+
+		public async Task Handle(AbandonReservationCommand message, IMessageHandlerContext context)
+		{
+			Log.Info($"Handle AbandonReservationCommand for reservation {message.ReservationUuid}");
+
+			var reservation = _reservationsContext.Reservations.Single(r => r.Uuid == message.ReservationUuid);
+			reservation.Status = "Abandoned";
+
+			await _reservationsContext.SaveChangesAsync();
+		}
+	}
+}
