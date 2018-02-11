e258b5cd811ba963ec9ff17db1eb7aa5bea6892c
Chris Morgan <cmorgan@inspirato.com>
2018-02-11 00:20:28 -0700

    Renames SPA application to SOA Hotel

--- src/PublicWebsite/src/app.ts
+++ src/PublicWebsite/src/app.ts
@@ -5,7 +5,7 @@ export class App {
   router: Router;
 
   configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Acme Hotel';
+    config.title = 'SOA Hotel';
 
 	config.map([
 		{ route: ['', 'branding'], name: 'branding', moduleId: './branding/findRooms', nav: true, title: 'Home' },
--- src/PublicWebsite/src/main.ts
+++ src/PublicWebsite/src/main.ts
@@ -14,20 +14,6 @@ export async function configure(aurelia: Aurelia) {
     .standardConfiguration()
     .developmentLogging();
 
-  // Uncomment the line below to enable animation.
-  // aurelia.use.plugin('aurelia-animator-css');
-  // if the css animator is enabled, add swap-order="after" to all router-view elements
-
-  // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  // aurelia.use.plugin('aurelia-html-import-template-loader')
-
   await aurelia.start();
   aurelia.setRoot('app');
-
-  // if you would like your website to work offline (Service Worker), 
-  // install and enable the @easy-webpack/config-offline package in webpack.config.js and uncomment the following code:
-  /*
-  const offline = await System.import('offline-plugin/runtime');
-  offline.install();
-  */
 }
